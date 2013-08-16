483996fb30b1e4afc7e50ed47d8be9cab1d0d717
AsakuraFuuko <asakurafuko@gmail.com>
2013-08-16 11:45:01 +0800

    添加ListBoxItem,BooleanStringConverter

---
+++ OSUTool/Lib/BooleanStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace OSUTool.Lib
+{
+    public class BooleanStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Boolean date = (Boolean)value;
+            if (date.Equals(false))
+            {
+                return "UnRanked";
+            }
+            else
+            {
+                return "Ranked";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OSUTool/Model/ListBoxItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace OSUTool.Model
+{
+    internal class ListBoxItem
+    {
+        public string id { get; set; }
+        public string bmpic { get; set; }
+        public string artist { get; set; }
+        public string title { get; set; }
+        public string mapper { get; set; }
+        public string from { get; set; }
+        public bool isRanked { get; set; }
+        public string date { get; set; }
+
+        public ObservableCollection<ListBoxItem> LVDatas;
+
+        public ListBoxItem()
+        {
+            LVDatas = new ObservableCollection<ListBoxItem>();
+        }
+    }
+}
