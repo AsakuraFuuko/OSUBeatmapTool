410ae4b806b9a4d05f268cc0f77e4c050bbe5b00
AsakuraFuuko <asakurafuko@gmail.com>
2013-08-16 12:37:29 +0800

    添加LogManager

---
+++ OSUBeatmapTool/Lib/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OSUBeatmapTool.Lib
+{
+    public static class Log
+    {
+        private static LogManager logManager;
+        static Log()
+        {
+            logManager = new LogManager();
+        }
+
+        public static void WriteLog(LogFile logFile, string msg)
+        {
+            logManager.WriteLog(logFile, msg);
+        }
+
+        public static void WriteLog(string logFile, string msg)
+        {
+            logManager.WriteLog(logFile, msg);
+        }
+    }
+}
